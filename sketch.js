d0aaf5135bc0b5ff52c3bf465b3e58465fdd8e4e
Rudzee30 <74777145+Rudzee30@users.noreply.github.com>
2021-05-08 21:54:14 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
 var PLAY = 1;
 var END = 0;
 var gamestate = PLAY;
-var mo,mon,ba,banana,ro,rock,b1,back,ig,bagroup,rogroup,mon1,go,game;
+var dislpayWidth=1900;
+var displayHeight=1000;
+var canvas ,mo,mon,ba,banana,ro,rock,b1,back,ig,bagroup,rogroup,mon1,go,game;
 
 
 
@@ -10,31 +12,33 @@ function preload(){
   mon=loadAnimation("Monkey_01.png","Monkey_02.png","Monkey_03.png","Monkey_04.png","Monkey_05.png","Monkey_06.png","Monkey_07.png","Monkey_08.png","Monkey_09.png","Monkey_10.png");
   mon1=loadAnimation("Monkey_01.png");
   banana = loadImage("banana.png");
-  back=loadImage("jungle.png");
+  back=loadImage("jungle1.png");
   rock=loadImage("stone.png");
   game=loadImage("gameOver.png")
 }
 
 
 function setup() {
-  createCanvas(400, 400);
+  canvas= createCanvas(dislpayWidth, displayHeight-150);
   
-  b1=createSprite(200,200,5,5);
+  b1=createSprite(dislpayWidth/2,(displayHeight-150)/2,5,5);
   b1.addImage(back);
   b1.x = b1.width /2;
+  b1.scale=2;
   
-  mo = createSprite(100, 350,50,50);
+  mo = createSprite(dislpayWidth/4, (displayHeight-150)-150,50,50);
   mo.addAnimation("running",mon);
   mo.addAnimation("collided",mon1);
-  mo.scale=0.1;
+  mo.scale=0.2;
   mo.setCollider("circle",0,0,200);
+  //camera.position.x=mo.x;
+  //camera.position.y=mo.y;
   
-  
-  ig = createSprite(200, 380,400,10);
+  ig = createSprite(dislpayWidth/2, 885,400,10);
   ig.visible=false;
   score = 0;
   
-  go=createSprite(200,200,5,5);
+  go=createSprite(dislpayWidth/2,(displayHeight-150)/2,5,5);
   go.addImage(game);
   go.visible=false;
   
@@ -51,7 +55,7 @@ function draw() {
      b1.x = b1.width/2;
    }
     
-   if(keyDown("space")&& mo.y>=300 ){
+   if(keyDown("space")&& mo.y>=displayHeight-100 ){
       mo.velocityY = -12 ;
       
     }
@@ -100,15 +104,15 @@ function draw() {
   ba1();
   drawSprites();
   fill("White");
-  text("Score: "+ score, 300,50);
+  text("Score: "+ score, 300,500);
 }
 function ro1(){
   if (World.frameCount%200===0) {
-    ro = createSprite(400, 400,5,5);
+    ro = createSprite(dislpayWidth, (displayHeight-150),5,5);
     ro.addImage(rock);
-    ro.scale=0.5; 
+    ro.scale=0.8; 
     ro.velocityX=-6;
-    ro.lifetime=107;
+    ro.lifetime=475;
     ro.setCollider("rectangle",0,0,100,300);
     rogroup.add(ro);
     
@@ -117,12 +121,12 @@ function ro1(){
 }
 function ba1(){
   if (World.frameCount% 80===0) {
-    ba = createSprite(400,0,5,5);
-    ba.y=random(170,230);
-    ba.scale=0.05;
+    ba = createSprite(dislpayWidth,0,5,5);
+    ba.y=random(((displayHeight-150)/2)-30,((displayHeight-150)/2)+30);
+    ba.scale=0.1;
     ba.addImage(banana);
     ba.velocityX=-8;
-    ba.lifetime=102;
+    ba.lifetime=475;
     bagroup.add(ba);
   }
 }
